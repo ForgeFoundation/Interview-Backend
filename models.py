7667153805e5cb37358449078f26810383e5f18a
NelsonWang <wangnelson2@gmail.com>
2024-02-10 11:32:06 -0500

    rough models design

--- models.py
+++ models.py
@@ -25,79 +25,83 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    guid = Column(String, nullable=False, server_default=func.gen_random_uuid())
+    firebaseid = Column(String, nullable=True)
     username = Column(String, nullable=True)
-    password = Column(String, nullable=True)
     email = Column(String, nullable=False)
-    tags = Column(ARRAY(String))
-    languages = Column(ARRAY(String))
     last_active = Column(DateTime, default=func.now(), nullable=False)
 
 
 
 class CreateUser(BaseModel):
+    firebaseid: str
     username: str
     email: str
-    tags: List[str]
-    languages: List[str]
-
     class Config:
         orm_mode = True
 
-class ViewUser(BaseModel):
+class ViewUser(BaseModel):   
     id: int
+    firebaseid: str
     username: str
     email: str
-    tags: List[str]
-    languages: List[str]
     last_active: datetime.datetime
-
     class Config:
         orm_mode = True
 
 
+class Prompt(Base):
+    __tablename__ = 'prompts'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    prompt = Column(String, nullable=False)
+    tags = Column(ARRAY(String), nullable=True)
+    description = Column(String, nullable=True)
+    hint = Column(String, nullable=True)
+
+class Answer(Base):
+    __tablename__ = 'answers'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    answer = Column(String, nullable=False)
+
+    prompt_message = Column(String, nullable=False) # It could be assigned to an non idead prompt.
+    promptid = Column(Integer, nullable=True)
+    
+    user_fid = Column(String, nullable=True)
+
+    votes = Column(Integer, nullable=True)
+    how_confident = Column(Integer, nullable=True)
 
 
-# Define the Thread model
-class Thread(Base):
-    __tablename__ = 'threads'
+    
+    tags = Column(ARRAY(String), nullable=True)
+    last_feedback_id = Column(Integer, nullable=True)
 
-    thread_id = Column(Integer, primary_key=True, autoincrement=True)
-    parent_id = Column(Integer, ForeignKey('threads.thread_id'), nullable=True)
-    is_forum = Column(Boolean, nullable=False)
-    guid = Column(String, nullable=False, server_default=func.gen_random_uuid())
-    title = Column(String, nullable=False)
-    body = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    created_time = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
-    tags = Column(ARRAY(String), nullable=False)
+    is_public = Column(Boolean, nullable=True)    
+    timestamp = Column(DateTime, default=func.now(), nullable=False)
 
 
+class Collections(Base):
+    __tablename__ = 'collections'
 
-class CreateForum(BaseModel):
-    title: str
-    body: str
-    user_id: int
-    tags: List[str]
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    label = Column(String, nullable=False)
+    description = Column(String, nullable=True)
+    image = Column(String, nullable=True)
+    tags = Column(ARRAY(String), nullable=True)
 
+class Tags(Base):
+    __tablename__ = 'tags'
 
-class CreateForumEmail(BaseModel):
-    title: str
-    body: str
-    email: str
-
-
-class CreateComment(BaseModel):
-    body: str
-    title: str
-    user_id: int
-    parent_id: int
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tag = Column(String, nullable=False)
+    description = Column(String, nullable=True)
 
 
-class CreateCommentWithEmail(BaseModel):
-    body: str
-    title: str
-    email: str
-    parent_id: int
-
-
+class Feedback(Base):
+    __tablename__ = 'feedback'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    answer_id = Column(Integer, ForeignKey('answers.id'), nullable=False)
+    type = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+    timestamp = Column(DateTime, default=func.now(), nullable=False)
